2e57b2dcac29d52cad3fdf8cee08d61be4d10faf
aliyan jabbar <jabbaraliyan805@gmail.com>
2025-07-17 16:37:09 +0500

    adding 12_structured_output

---
+++ 12_structured_output/main.py
@@ -0,0 +1,57 @@
+from pydantic import BaseModel
+from agents import (
+    Agent,
+    Runner,
+    AsyncOpenAI,
+    OpenAIChatCompletionsModel,
+    set_tracing_disabled,
+)
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+gemini_api_key = os.getenv("GEMINI_API_KEY")
+
+
+# Check if the API key is present; if not, raise an error
+if not gemini_api_key:
+    raise ValueError(
+        "GEMINI_API_KEY is not set. Please ensure it is defined in your .env file."
+    )
+
+
+# Reference: https://ai.google.dev/gemini-api/docs/openai
+external_client = AsyncOpenAI(
+    api_key=gemini_api_key,
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/",
+)
+
+external_model = OpenAIChatCompletionsModel(
+    model="gemini-2.0-flash", openai_client=external_client
+)
+
+set_tracing_disabled(True)
+
+
+class WeatherAnswer(BaseModel):
+    location: str
+    temperature_c: float
+    summary: str
+    details: str
+
+
+agent = Agent(
+    name="StructuredWeatherAgent",
+    instructions="Use the final_output tool with WeatherAnswer schema.",
+    output_type=WeatherAnswer,
+    model=external_model,
+)
+
+
+city_input = input("enter city to get temperature: ")
+
+res = Runner.run_sync(agent, f"What's the temperature in {city_input}?")
+print("type of output: ", type(res.final_output))
+print("final output: ", (res.final_output))
